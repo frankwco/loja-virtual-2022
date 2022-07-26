dd341768dd9470fdc65755bcce256176c8a3d52b
Frank W. C. de Oliveira <frankwco@gmail.com>
2022-07-26 17:36:38 -0300

    iniciando

---
+++ backend/src/main/java/com/dev/backend/controller/Hello.java
@@ -0,0 +1,18 @@
+package com.dev.backend.controller;
+
+import java.util.Date;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class Hello {
+
+    @GetMapping("/")
+    public String hello(){
+        return "Olá mundo Spring "+new Date();
+    }
+    
+}
