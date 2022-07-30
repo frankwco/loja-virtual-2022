51996873b90cdaea4e2e3b3697709f4341d2e47b
Frank W. C. de Oliveira <frankwco@gmail.com>
2022-07-30 18:49:03 -0300

    crud estado

---
+++ backend/src/main/java/com/dev/backend/controller/EstadoController.java
@@ -0,0 +1,49 @@
+package com.dev.backend.controller;
+
+import java.util.List;
+
+import javax.websocket.server.PathParam;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.dev.backend.entity.Estado;
+import com.dev.backend.service.EstadoService;
+
+@RestController
+@RequestMapping("/api/estado")
+public class EstadoController {
+    
+    @Autowired
+    private EstadoService estadoService;
+
+    @GetMapping("/")
+    public List<Estado> buscarTodos(){
+       return estadoService.buscarTodos();
+    }
+
+    @PostMapping("/")
+    public Estado inserir(@RequestBody Estado estado){
+        return estadoService.inserir(estado);
+    }
+
+    @PutMapping("/")
+    public Estado alterar(@RequestBody Estado estado){
+        return estadoService.alterar(estado);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> excluir(@PathVariable("id") Long id){
+        estadoService.excluir(id);
+        return ResponseEntity.ok().build();
+    }
+
+}
---
+++ backend/src/main/java/com/dev/backend/service/EstadoService.java
@@ -0,0 +1,37 @@
+package com.dev.backend.service;
+
+import java.util.Date;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.dev.backend.entity.Estado;
+import com.dev.backend.repository.EstadoRepository;
+
+@Service
+public class EstadoService {
+
+    @Autowired
+    private EstadoRepository estadoRepository;
+
+    public List<Estado> buscarTodos() {
+        return estadoRepository.findAll();
+    }
+
+    public Estado inserir(Estado estado) {
+        estado.setDataCriacao(new Date());
+        Estado estadoNovo = estadoRepository.saveAndFlush(estado);
+        return estadoNovo;
+    }
+
+    public Estado alterar(Estado estado) {
+        estado.setDataAtualizacao(new Date());
+        return estadoRepository.saveAndFlush(estado);
+    }
+
+    public void excluir(Long id) {
+        Estado estado = estadoRepository.findById(id).get();
+        estadoRepository.delete(estado);
+    }
+}
